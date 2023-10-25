750b675a7cb4b0d6979da0b0a9703fb515f0896f
G. Willson <greg@biomassiv.es>
2023-10-25 16:53:26 -0600

    Update QuoteCard.vue

--- components/cards/QuoteCard.vue
+++ components/cards/QuoteCard.vue
@@ -2,14 +2,13 @@
   <div class="card quote-card">
     <div class="card-content">
       <p class="title">
-        “Always code as if the guy who ends up maintaining your code will be a
-        violent psychopath who knows where you live.”
+        “This activity could quality for funding support from crowdfunding and direct biodiversity credit sales.”
       </p>
       <p class="subtitle">
-        ~ Martin Golding
+        ~ G W Willson
       </p>
     </div>
-    <footer class="card-footer">
+    <footer class="card-footer">`
       <div class="card-footer-item">
         <span> Share on <a href="#">Twitter</a> </span>
       </div>
