4b59a3e39d28118712d09b2342d0a73e6562e047
G. Willson <greg@biomassiv.es>
2023-11-21 22:00:49 -0700

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -1,53 +1,13 @@
 <template>
-  <div class="logo-wrapper">
-
-      <img
-        :src="/assets/uploads/kigi.gif"
-      />
-
-  </div>
+    <div id="app">
+        <img :src="require('./assets/kigi.gif')"/>
+    </div>
 </template>
+    
 <script>
-export default {
-  name: 'SiteLogo',
-  data() {
-    return {
-      frames: 5,
-      currentFrame: 1,
-      blinking: true
+    export default {
     }
-  },
-  mounted() {
-    setInterval(() => {
-      const rand = Math.ceil(Math.random() * 10)
-      this.blinking = rand % 2
-    }, 1000)
-  }
-}
 </script>
-<style lang="scss">
-@keyframes logoBlink {
-  100% {
-    transform: translateY(-360px);
-  }
-}
-.logo-wrapper {
-  position: relative;
-  height: 50px;
-  overflow: hidden;
-  width: 200px;
-  .logo-inner {
-    position: absolute;
-    height: 500px;
-    img {
-      display: block;
-      height: 40px;
-      margin: 5px;
-      max-height: initial;
-    }
-    &.blink {
-      animation: logoBlink 0.2s steps(8, end) alternate;
-    }
-  }
-}
+    
+<style lang="css">
 </style>
