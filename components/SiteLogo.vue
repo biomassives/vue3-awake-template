15caa4bbe64ef2796ca410ade885e4ead1b403ec
G. Willson <greg@biomassiv.es>
2023-11-21 21:54:16 -0700

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="logo-wrapper">
-    <div
-      :class="{
-        'logo-inner': true,
-        blink: blinking
-      }"
-    >
+
       <img
-        v-for="x in frames"
-        :key="x"
-        :src="`/logo-frames/logo-f${x - 1}.svg`"
+        :src="/assets/uploads/kigi.gif"
       />
-      <img
-        v-for="x in frames"
-        :key="`${x}-r`"
-        :src="`/logo-frames/logo-f${frames - x}.svg`"
-      />
-    </div>
+
   </div>
 </template>
 <script>
