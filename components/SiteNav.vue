3c000c69fea718777c9e44679af13f6d2e125523
G. Willson <greg@biomassiv.es>
2023-11-24 10:18:09 -0700

    Update SiteNav.vue

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -5,15 +5,7 @@
     aria-label="main navigation"
   >
     <div class="navbar-brand">
-      <nuxt-link class="navbar-item" to="/">
-        
-        <img
-          v-else
-          :src="$siteConfig.logo"
-          :alt="$siteConfig.siteName"
-          class="logo"
-        />
-      </nuxt-link>
+
       <hamburger-button @click="active = !active" />
     </div>
 
